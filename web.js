8f1b715885f6c0aa478b42123ff2bc88375806d6
Dexter Zhuang <dexter.zhuang@gmail.com>
2013-07-10 06:20:40 +0000

    added require fs

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var hello_txt = fs.readFileSync('index.html');
+var hello_str = hello_txt.toString('utf-8');
 
 var app = express.createServer(express.logger());
  
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello_str);
 });
 
 var port = process.env.PORT || 5000;
