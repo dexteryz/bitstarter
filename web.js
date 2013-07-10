d6abba6ee158aa3c54bffca126bce5d2af5d5ecf
Dexter Zhuang <dexter.zhuang@gmail.com>
2013-07-10 06:04:23 +0000

    fixed web.js again

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var hello_txt = fs.readFileSync('index.html');
+var hello_str = hello_txt.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(hello_txt.toString('utf-8'));
+  response.send(hello_str);
 });
 
 var port = process.env.PORT || 5000;
